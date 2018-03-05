8bf20d13b212cae1923166ebab78df8075e5e1dd
Lucas Nogueira Mesquita <lnmesquita1@gmail.com>
2018-03-05 16:10:34 -0300

    Alterações das configurações de conexão

--- util/Conecta.php
+++ util/Conecta.php
@@ -5,7 +5,7 @@ class Conecta {
     private $conexao;
     
     function __construct() {
-         $this->conexao = mysqli_connect('localhost', 'root', 'lucas', 'relogio');
+         $this->conexao = mysqli_connect('localhost', 'root', '', 'relogio');
     }
     
     function getConexao() {       
